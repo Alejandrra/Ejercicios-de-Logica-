56b1c10c6113bae939d3b5282814695fac15a352
Alejandra <50176363+Alejandrra@users.noreply.github.com>
2025-05-13 12:56:39 -0500

    ejercicio realizado

---
+++ Numeros_primos.py
@@ -0,0 +1,15 @@
+""""Escribe un programa que se encargue de comprobar si un 
+    numero es primo o no.
+    Hecho esto, imprime los numeros primos entre 1 y 100
+ """""""""
+def num_primos(n):
+    if n <= 1:
+        return False  
+
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False  
+    return True  
+
+print(num_primos(4))
